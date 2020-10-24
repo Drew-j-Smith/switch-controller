702f47a5c36fe238a93a219cb80c8c0798b82de1
Drew Smith <smithdrew555@yahoo.com>
2020-10-24 14:26:39 -0400

    added command line arg for config

--- windows client/Arduino.cpp
+++ windows client/Arduino.cpp
@@ -4,13 +4,24 @@
 
 #include <iostream>
 #include <thread>
+#include <string>
 #include "VirtualController.h"
 #include "ImgProc.h"
 #include "ArduinoLoader.h"
 
 
-int main(){
-	ArduinoLoader l("config.json");
+int main(int argc, char **argv){
+
+	string config = "config.json";
+
+	for(int i = 1; i < argc; i++){
+		if(strcmp(argv[i], "-c") == 0 && argc > i){
+			config = string(argv[i + 1]);
+			cout << "Using config \"" << config << "\"\n";
+		}
+	}
+
+	ArduinoLoader l(config);
 	ImgProc i(l.getPictures(), l.getMacros(), l.getOption("window name"), stoi(l.getOption("window width")), stoi(l.getOption("window height")));
 	VirtualController v(l.getPictures(), l.getMacros(), l.getSwitchButtons(), l.getOption("serial port"), l.getOption("macro folder"));
 
