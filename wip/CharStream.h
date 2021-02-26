0f287eb758c97017bf7766aeda41877887b44e58
Drew Smith <smithdrew555@yahoo.com>
2021-02-26 12:18:38 -0500

    added implementation for CharStream

--- wip/CharStream.h
+++ wip/CharStream.h
@@ -3,12 +3,98 @@
 
 #include "pch.h"
 
+
+
 template<size_t frameSize>
 class CharStream : public std::vector<std::array<unsigned char, frameSize>>{
+private:
+    int hexToInt(char t){
+        if(isdigit(t))
+            return t - '0';
+        else
+            return tolower(t) - 'a' + 10;
+    }
 public:
-    void load(const std::string & filename, bool isHex = true);
+    void load(const std::string & filename, const bool isHex = true){
+        clear();
+        reserve(10000);
+        if (isHex){
+            std::ifstream infile(filename, std::ios::in | std::ios::binary);
+            if (infile) {
+                std::array<unsigned char, frameSize> buffer;
+                char currentChar;
+                while (!infile.eof()) {
+                    int i;
+                    for (i = 0; i < frameSize * 2; i++) {
+                        infile >> currentChar;
+                        if (!isxdigit(currentChar))
+                            break;
+                        if (i % 2 == 0)
+                            buffer[i/2] = hexToInt(currentChar) * 16;
+                        else
+                            buffer[i/2] += hexToInt(currentChar);
+                    }
+                    if (i != frameSize * 2){
+                        std::cerr << "Non-hex character in file \"" << filename << "\"\n";
+                        break;
+                    }
+                    if (infile.good())
+                        push_back(buffer);
+                }
+            }
+            else {
+                std::cerr << "Error opening file \"" << filename << "\"\n";
+            }
+            infile.close();
+        }
+        else {
+            std::ifstream infile(filename, std::ios::in | std::ios::binary);
+            if (infile) {
+                std::array<unsigned char, frameSize> buffer;
+                while (!infile.eof()) {
+                    infile.read((char*)buffer.data(), frameSize);
+                    if (infile.good())
+                        push_back(buffer);
+                }
+            }
+            else {
+                std::cerr << "Error opening file \"" << filename << "\"\n";
+            }
+            infile.close();
+        }
+        shrink_to_fit();
+    }
 
-    void save(const std::string & filename, bool asHex = true);
+    void save(const std::string & filename, const bool asHex = true){
+        if (asHex){
+            std::ofstream outfile(filename, std::ios::out);
+            if (outfile) {
+                outfile << std::hex << std::setfill('0');
+                for (int i = 0; i < size(); i++) {
+                    for (int j = 0; j < frameSize; j++) {
+                        outfile << std::setw(2) << (int)at(i).at(j);
+                    }
+                    outfile << std::endl;
+                }
+            }
+            else {
+                std::cerr << "Error writing to file \"" << filename << "\"\n";
+            }
+            outfile.close();
+        }
+        else {
+            std::ofstream outfile(filename, std::ios::out | std::ios::binary);
+            if (outfile) {
+                for (int i = 0; i < size(); i++) {
+                    outfile.write((char*)at(i).data(), frameSize);
+                }
+            }
+            else {
+                std::cerr << "Error writing to file \"" << filename << "\"\n";
+            }
+            outfile.close();
+        }
+    }
 };
 
 
--- wip/test.cpp
+++ wip/test.cpp
@@ -13,6 +13,29 @@ int main(){
     a.push_back(b);
     std::cout << a[0][0] << std::endl;
 
+    a.save("test.txt", true);
+
+    CharStream<16> c;
+    c.load("test2.txt");
+    std::cout << c.size() << std::endl;
+    std::cout << std::hex << std::setfill('0');
+    // for (int i = 0; i < c.size(); i++) {
+    //     for (int j = 0; j < 16; j++) {
+    //         std::cout << std::setw(2) << (int)c.at(i).at(j);
+    //     }
+    //     std::cout << std::endl;
+    // }
+    c.save("test3.txt");
+
+    c.save("test4.txt", false);
+
+    c.load("test4.txt", false);
+    for (int i = 0; i < c.size(); i++) {
+        for (int j = 0; j < 16; j++) {
+            std::cout << std::setw(2) << (int)c.at(i).at(j);
+        }
+        std::cout << std::endl;
+    }
 
 
     return 0;
