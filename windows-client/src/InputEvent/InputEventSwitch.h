b38f9909d1a40f05e1ed5b1fc910d0fabce20788
Drew Smith <smithdrew555@yahoo.com>
2021-10-27 00:12:09 -0400

    added argument checking to input event switch

--- windows-client/src/InputEvent/InputEventSwitch.h
+++ windows-client/src/InputEvent/InputEventSwitch.h
@@ -19,6 +19,10 @@ public:
         this->deciderEvent = event1;
     }
     InputEventSwitch(std::shared_ptr<InputEvent> event1, std::shared_ptr<InputEvent> event2, std::shared_ptr<InputEvent> deciderEvent) {
+        if (event1->isDigital() != event2->isDigital() || !deciderEvent->isDigital()) {
+            throw std::invalid_argument("Invalid input types for InputEventSwitch: " +
+            event1->getTypeName() + " " + event2->getTypeName() + " " + deciderEvent->getTypeName());
+        }
         this->event1 = event1;
         this->event2 = event2;
         this->deciderEvent = deciderEvent;
@@ -31,7 +35,7 @@ public:
             return event2->getInputValue();
     }
 
-    bool isDigital() const override { return false; }
+    bool isDigital() const override { return event1->isDigital(); }
 
     std::shared_ptr<InputEvent> makeShared(const boost::property_tree::ptree & tree, const std::map<std::string, std::shared_ptr<InputEvent>> & eventMap) const override { return nullptr; }
     std::string getTypeName() const override { return ""; }
