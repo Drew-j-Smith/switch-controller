f578203d823e65471b6801549740c14fe9eb44c1
Drew Smith <smithdrew555@yahoo.com>
2021-10-29 22:19:55 -0400

    linked opencv

--- ffmpeg-test/test1.cpp
+++ ffmpeg-test/test1.cpp
@@ -29,6 +29,11 @@
  * @example demuxing_decoding.c
  */
  
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
  extern "C" {
     #include <stdint.h>
     #include <libavutil/imgutils.h>
