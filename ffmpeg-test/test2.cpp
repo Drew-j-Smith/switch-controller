329f8b801aa80be5af8a4427e6d2569cd01125dd
Drew Smith <smithdrew555@yahoo.com>
2021-11-04 21:33:58 -0400

    improved performance by using bgr24 format

--- ffmpeg-test/test2.cpp
+++ ffmpeg-test/test2.cpp
@@ -8,7 +8,7 @@
 int main() {
     std::string inputFormat = "dshow";
     std::string deviceName = "video=Game Capture HD60 S";
-    std::map<std::string, std::string> options;
+    std::map<std::string, std::string> options = {{"pixel_format", "bgr24"}};
     std::vector<std::shared_ptr<FfmpegFrameSink>> sinks;
     std::shared_ptr<FfmpegFrameSink> videoSink = std::make_shared<VideoFrameSink>();
     sinks.push_back(videoSink);
