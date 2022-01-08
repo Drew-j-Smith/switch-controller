358eb50d460ba3bde19e0dcd24954218353a494e
Drew Smith <smithdrew555@yahoo.com>
2022-01-08 15:49:12 -0500

    added build to gitignore and replaced \ with /

--- windows-client/list_tests.py
+++ windows-client/list_tests.py
@@ -1,7 +1,7 @@
 import os
 
-os.system(".\\windows-client\\build\\test\\Debug\\ArduinoControllerTest.exe --list_content --detect_memory_leaks=0 2> windows-client/build/test/Debug/py-output")
+os.system("./build/test/Debug/ArduinoControllerTest.exe --list_content --detect_memory_leaks=0 2> windows-client/build/test/Debug/py-output")
 
-with open("windows-client/build/test/Debug/py-output") as f:
+with open("build/test/Debug/py-output") as f:
     [print(f"add_test(NAME {test} COMMAND ArduinoControllerTest --detect_memory_leaks=0 --run_test={test})") for test in \
         [line.replace("*", "").replace("\n", "") for line in f if not line.startswith(" ")]]
