ccfb67db35abb8ebe11963808b855474e4baa129
Drew Smith <smithdrew555@yahoo.com>
2022-01-16 21:00:25 -0500

    fixed warning about unreference parameter

--- windows-client/src/InputEvent/DefaultInputEvent.h
+++ windows-client/src/InputEvent/DefaultInputEvent.h
@@ -27,6 +27,10 @@ public:
     bool isDigital() const override { return true; }
 
     std::shared_ptr<InputEvent> makeShared(const boost::property_tree::ptree & tree, const std::map<std::string, std::shared_ptr<InputEvent>> & eventMap) const override { 
+        // Unused parameters
+        tree;
+        eventMap;
+        
         return std::make_shared<DefaultInputEvent>(); 
     }
     std::string getTypeName() const override { return "DefaultInputEvent"; }
@@ -54,6 +58,10 @@ public:
     bool isDigital() const override { return true; }
 
     std::shared_ptr<InputEvent> makeShared(const boost::property_tree::ptree & tree, const std::map<std::string, std::shared_ptr<InputEvent>> & eventMap) const override { 
+        // Unused parameters
+        tree;
+        eventMap;
+        
         return std::make_shared<ActiveInputEvent>(); 
     }
     std::string getTypeName() const override { return "ActiveInputEvent"; }
