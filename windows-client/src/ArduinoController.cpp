Drew-j-Smith/switch-controller

commit 2f47b14f151a1ed7b9a1a5bab30003ef843aefab

fixed warnings with ffmpeg

windows-client/src/FFmpeg/FFmpegRecorder.cpp
@@ -13,7 +13,16 @@
 }
 
 FFmpegRecorder::FFmpegRecorder(std::string inputFormat, std::string deviceName, std::map<std::string, std::string> options, std::vector<std::shared_ptr<FFmpegFrameSink>> sinks) {
-    if (checkOverlap(sinks)) {
+    std::set<AVMediaType> types;
+    bool overlap = false;
+    for (auto sink : sinks) {
+        if (types.find(sink->getType()) == types.end()) {
+            types.insert(sink->getType());
+        } else {
+            overlap = true;
+        }
+    }
+    if (overlap) {
         std::string errStr = "Only one frame sink of each type may be used";
         std::cerr << errStr << '\n';
         throw std::runtime_error(errStr);
@@ -23,23 +32,11 @@ FFmpegRecorder::FFmpegRecorder(std::string inputFormat, std::string deviceName, 
         std::cerr << errStr << '\n';
         throw std::runtime_error(errStr);
     }
-    
-    this->inputFormat = inputFormat;
-    this->deviceName = deviceName;
+
+    this->inputFormatStr = inputFormat;
+    this->deviceNameStr = deviceName;
     this->options = options;
     this->sinks = sinks;
-}
-
-bool FFmpegRecorder::checkOverlap(std::vector<std::shared_ptr<FFmpegFrameSink>> sinks) {
-    std::set<AVMediaType> types;
-    for (auto sink : sinks) {
-        if (types.find(sink->getType()) == types.end()) {
-            types.insert(sink->getType());
-        } else {
-            return true;
-        }
-    }
-    return false;
 }
 
 void FFmpegRecorder::free() {
@@ -49,7 +46,7 @@ void FFmpegRecorder::free() {
     av_frame_free(&frame);
 }
 
-void FFmpegRecorder::openStream(std::string inputFormatStr, std::string deviceName, std::map<std::string, std::string> optionsMap, std::vector<std::shared_ptr<FFmpegFrameSink>> sinks) {
+void FFmpegRecorder::openStream() {
     avdevice_register_all();
 
     // finding input format and setting options dict
@@ -57,15 +54,15 @@ void FFmpegRecorder::openStream(std::string inputFormatStr, std::string deviceNa
     if (inputFormatStr.size() > 0) { // If no input format is set, load a file
         inputFormat = av_find_input_format(inputFormatStr.c_str());
     }
-    AVDictionary* options = nullptr;
-    for (auto& pair : optionsMap) {
-        av_dict_set(&options, pair.first.c_str(), pair.second.c_str(), 0);
+    AVDictionary* optionsDic = nullptr;
+    for (auto& pair : options) {
+        av_dict_set(&optionsDic, pair.first.c_str(), pair.second.c_str(), 0);
     }
  
     // open input file, and allocate format context
-    if (avformat_open_input(&formatContext, deviceName.c_str(), inputFormat, &options) < 0) {
+    if (avformat_open_input(&formatContext, deviceNameStr.c_str(), inputFormat, &optionsDic) < 0) {
         free();
-        std::string errStr = "Could not open stream " + deviceName + " in FFmpeg Recorder";
+        std::string errStr = "Could not open stream " + deviceNameStr + " in FFmpeg Recorder";
         std::cerr << errStr << '\n';
         throw std::runtime_error(errStr);
     }
@@ -73,21 +70,15 @@ void FFmpegRecorder::openStream(std::string inputFormatStr, std::string deviceNa
     // retrieve stream information
     if (avformat_find_stream_info(formatContext, nullptr) < 0) {
         free();
-        std::string errStr = "Could not find stream information for " + deviceName + " in FFmpeg Recorder";
+        std::string errStr = "Could not find stream information for " + deviceNameStr + " in FFmpeg Recorder";
         std::cerr << errStr << '\n';
         throw std::runtime_error(errStr);
     }
     
     // open stream ctx for each frame sink
     for (auto sink : sinks) {
-        try {
-            std::shared_ptr<FFmpegDecoder> decoder = std::make_shared<FFmpegDecoder>(formatContext, sink);
-            decoders.insert({decoder->getStreamIndex(), decoder});
-        }
-        catch (std::exception& e) {
-            free();
-            throw;
-        }
+        std::shared_ptr<FFmpegDecoder> decoder = std::make_shared<FFmpegDecoder>(formatContext, sink);
+        decoders.insert({decoder->getStreamIndex(), decoder});
     }
 
     if(decoders.size() == 0) {
@@ -105,7 +96,7 @@ void FFmpegRecorder::start() {
     recording.store(true);
 
     recordingThread = std::thread([&](){
-        openStream(inputFormat, deviceName, options, sinks);
+        openStream();
 
         AVPacket pkt;
         frame = av_frame_alloc();
@@ -119,29 +110,14 @@ void FFmpegRecorder::start() {
             // check if the pack goes to a frame sink
             if (decoders.find(pkt.stream_index) != decoders.end()) {
                 auto decoder = decoders.at(pkt.stream_index);
-                try {
-                    decoder->decodePacket(&pkt, frame);
-                }
-                catch (std::runtime_error& e) {
-                    free();
-                    throw;
-                }
-                
+                decoder->decodePacket(&pkt, frame);
             }
             av_packet_unref(&pkt);
         }
 
         // flushing decoders
         for (auto it : decoders) {
-            try { 
-                it.second->decodePacket(nullptr, frame);
-            }
-            catch (std::runtime_error& e) {
-                free();
-                throw;
-            }
+            it.second->decodePacket(nullptr, frame);
         }
-
-        free();
     });
 }

windows-client/src/FFmpeg/FFmpegRecorder.h
@@ -24,15 +24,12 @@ private:
     std::map<int, std::shared_ptr<FFmpegDecoder>> decoders;
 
     std::vector<std::shared_ptr<FFmpegFrameSink>> sinks;
-    std::string inputFormat; std::string deviceName; std::map<std::string, std::string> options;
+    std::string inputFormatStr; std::string deviceNameStr; std::map<std::string, std::string> options;
 
     std::atomic<bool> recording;
     std::thread recordingThread;
 
-    void openStream(std::string inputFormatStr, std::string deviceName, std::map<std::string, std::string> optionsMap, std::vector<std::shared_ptr<FFmpegFrameSink>> sinks);
-    void openCodecContext(int* streamIndex, AVCodecContext** decoderContext, AVFormatContext* formatContex, enum AVMediaType type);
-
-    bool checkOverlap(std::vector<std::shared_ptr<FFmpegFrameSink>> sinks);
+    void openStream();
 
     void free();
 public:

windows-client/src/FFmpeg/AudioFrameSink.h
@@ -45,7 +45,7 @@ public:
 
     void virtualInit(AVCodecContext* decoderContext) override {
         // getting the channel layout or setting it to default
-        int channel_layout = decoderContext->channel_layout;
+        uint64_t channel_layout = decoderContext->channel_layout;
         if (channel_layout == 0) {
             channel_layout = av_get_default_channel_layout(decoderContext->channels);
         }
@@ -69,7 +69,7 @@ public:
     }
 
     void virtualOutputFrame(AVFrame* frame) override {
-        int out_samples = av_rescale_rnd(swr_get_delay(swr, frame->sample_rate) + frame->nb_samples, 48000, frame->sample_rate, AV_ROUND_UP);
+        int64_t out_samples = av_rescale_rnd(swr_get_delay(swr, frame->sample_rate) + frame->nb_samples, 48000, frame->sample_rate, AV_ROUND_UP);
         uint8_t* output;
         int res;
 
@@ -80,7 +80,7 @@ public:
             output = data.data() + data.size() - out_samples * av_get_bytes_per_sample(outputSampleFormat);
         }
 
-        res = swr_convert(swr, &output, out_samples, (const uint8_t**)frame->extended_data, frame->nb_samples);
+        res = swr_convert(swr, &output, (int)out_samples, (const uint8_t**)frame->extended_data, frame->nb_samples);
         if (res < 0) {
             char error[AV_ERROR_MAX_STRING_SIZE];
             av_make_error_string(error, AV_ERROR_MAX_STRING_SIZE, res);
@@ -89,7 +89,7 @@ public:
         }
 
         if (loopRecording) {
-            if (loopBufferPosition + out_samples * av_get_bytes_per_sample(outputSampleFormat) >= data.size()) {
+            if (loopBufferPosition + out_samples * av_get_bytes_per_sample(outputSampleFormat) >= (int64_t)data.size()) {
                 std::copy(dataBuffer.begin(),
                           dataBuffer.begin() + (data.size() - loopBufferPosition),
                           data.begin() + loopBufferPosition);
@@ -106,11 +106,11 @@ public:
         }
     }
 
-    void getDataWithoutLock(std::vector<uint8_t>& data) override {
-        data.resize(this->data.size());
+    void getDataWithoutLock(std::vector<uint8_t>& dataCopy) override {
+        dataCopy.resize(this->data.size());
         if (loopRecording) {
-            std::copy(this->data.begin() + loopBufferPosition, this->data.end(), data.begin());
-            std::copy(this->data.begin(), this->data.begin() + loopBufferPosition, data.begin() + (this->data.size() - loopBufferPosition));
+            std::copy(this->data.begin() + loopBufferPosition, this->data.end(), dataCopy.begin());
+            std::copy(this->data.begin(), this->data.begin() + loopBufferPosition, dataCopy.begin() + (this->data.size() - loopBufferPosition));
         } else {
             std::copy(this->data.begin(), this->data.end(), data.begin());
         }

windows-client/src/FFmpeg/FFmpegFrameSink.h
@@ -55,9 +55,9 @@ public:
         return lastFrame;
     }
 
-    long long getNextData(std::vector<uint8_t>& data, long long lastFrame) {
+    long long getNextData(std::vector<uint8_t>& data, long long lastFrameSeen) {
         std::unique_lock<std::mutex> lock(dataMutex);
-        while (this->lastFrame == lastFrame) {
+        while (this->lastFrame == lastFrameSeen) {
             dataCV.wait(lock);
         }
         getDataWithoutLock(data);

windows-client/src/FFmpeg/VideoFrameSink.h
@@ -83,9 +83,9 @@ private:
         }
     }
 
-    void getDataWithoutLock(std::vector<uint8_t>& data) override {
-		data.resize(this->data.size());
-        std::copy(this->data.begin(), this->data.end(), data.begin());
+    void getDataWithoutLock(std::vector<uint8_t>& dataCopy) override {
+		dataCopy.resize(this->data.size());
+        std::copy(this->data.begin(), this->data.end(), dataCopy.begin());
     }
 
 public:

commit 9881da20a162e23fbc33282cb91ceaef8b5409cb

fixed a bug introduced in 2f47b14f151a1ed7b9a1a5bab30003ef843aefab related to freeing memory and an opencv error with imshow

windows-client/src/ArduinoController.cpp
@@ -96,11 +96,12 @@ int main(int argc, const char** argv)
                     cv::Mat mat = cv::Mat(videoSink->getHeight(), videoSink->getWidth(), CV_8UC3, data.data());
 
                     std::atomic<bool> running = true;
+                    std::string title = std::to_string(std::chrono::steady_clock::now().time_since_epoch().count());
 
                     std::thread t([&]() {
                         while (running.load()) {
                             lastFrame = videoSink->getNextData(data, lastFrame);
-                            cv::imshow("test", mat);
+                            cv::imshow(title, mat);
                             cv::waitKey(1);
                         }
                     });

windows-client/src/FFmpeg/FFmpegRecorder.cpp
@@ -99,6 +99,13 @@ void FFmpegRecorder::start() {
             for (auto it : decoders) {
                 it.second->decodePacket(nullptr, frame);
             }
+
+            // need to call these methods here... for some reason
+            // if these methods aren't called in this thread it crashes
+            decoders.clear();
+            avformat_close_input(&formatContext);
+            av_frame_free(&frame);
+
         } catch (std::exception& e) {
             std::cerr << e.what() << '\n';
             throw;

windows-client/src/FFmpeg/FFmpegRecorder.h
@@ -34,6 +34,7 @@ public:
     FFmpegRecorder(std::string inputFormat, std::string deviceName, std::map<std::string, std::string> options, std::vector<std::shared_ptr<FFmpegFrameSink>> sinks);
     ~FFmpegRecorder() {
         stop();
+        decoders.clear();
         avformat_close_input(&formatContext);
         av_frame_free(&frame);
      };