a6eb6e8aaa4926365efc172e7abaec1cb6c32f44
Drew Smith <smithdrew555@yahoo.com>
2022-02-20 15:08:33 -0500

    added newline between stacktrace and message

--- windows-client/src/ErrorTypes/ErrorBase.h
+++ windows-client/src/ErrorTypes/ErrorBase.h
@@ -8,10 +8,10 @@
 template <class T, class Base> class ErrorBase : public Base {
 public:
     ErrorBase(const char *message)
-        : Base(std::string(message) +
+        : Base(std::string(message) + "\n" +
                boost::stacktrace::to_string(boost::stacktrace::stacktrace())) {}
     ErrorBase(const std::string &message)
-        : Base(message +
+        : Base(message + "\n" +
                boost::stacktrace::to_string(boost::stacktrace::stacktrace())) {}
     ErrorBase(const ErrorBase &other) : Base(other) {}
 };
