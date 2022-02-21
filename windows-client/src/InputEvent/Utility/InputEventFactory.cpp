5d320d476fd26b48a1be590648fd6da0574ce487
Drew Smith <smithdrew555@yahoo.com>
2022-02-20 19:40:31 -0500

    moved private helper method to cpp file

--- windows-client/src/InputEvent/Utility/InputEventFactory.cpp
+++ windows-client/src/InputEvent/Utility/InputEventFactory.cpp
@@ -2,6 +2,20 @@
 #include "InputEventFactory.h"
 #include "InputEvent/InputEventTypes.h"
 
+template <class... classes>
+static void addClasses(
+    const std::vector<std::string> &names, InputEventFactory *factory,
+    std::map<std::string, boost::function<std::shared_ptr<InputEvent>(
+                              boost::property_tree::ptree)>> &factories) {
+    std::vector<boost::function<std::shared_ptr<InputEvent>(
+        boost::property_tree::ptree)>>
+        factoryVec = {boost::bind(boost::factory<std::shared_ptr<classes>>(),
+                                  _1, boost::ref(*factory))...};
+    for (int i = 0; i < (int)names.size(); i++) {
+        factories[names[i]] = factoryVec[i];
+    }
+}
+
 InputEventFactory::InputEventFactory(
     const std::map<std::string, boost::property_tree::ptree>
         &inputEventTemplates) {
@@ -11,5 +25,5 @@ InputEventFactory::InputEventFactory(
             boost::bind(boost::factory<std::shared_ptr<InputEventTemplate>>(),
                         templateString.second, _1, boost::ref(*this));
     }
-    addClasses<AC_INPUT_EVENT_TYPES>(names);
+    addClasses<AC_INPUT_EVENT_TYPES>(names, this, this->factories);
 }
